b074e7b32024998d12ce67b13d7fcc5075270233
Jailton Mendes <jailtonmendes.silva@gmail.com>
2023-04-28 15:01:28 -0300

    Subindo projeto para o Git

---
+++ lib/setState/imc_Setstate_page.dart
@@ -0,0 +1,126 @@
+import 'dart:math';
+
+import 'package:app/widgets/imc_gauge.dart';
+import 'package:currency_text_input_formatter/currency_text_input_formatter.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class ImcSetstatePage extends StatefulWidget {
+  const ImcSetstatePage({Key? key}) : super(key: key);
+
+  @override
+  State<ImcSetstatePage> createState() => _ImcSetstatePageState();
+}
+
+class _ImcSetstatePageState extends State<ImcSetstatePage> {
+  final pesoEC = TextEditingController();
+  final alturaEC = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+  var imc = 0.0;
+
+  Future<void> _calcularIMC(
+      {required double peso, required double altura}) async {
+    setState(() {
+      imc = 0;
+    });
+
+    await Future.delayed(const Duration(seconds: 1));
+
+    setState(() {
+      imc = peso / pow(altura, 2); //Calcular imc = Peso ao quadrado
+    });
+  }
+
+  @override
+  void dispose() {
+    pesoEC.dispose();
+    alturaEC.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print('Build TELA');
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Imc SetState'),
+      ),
+      body: SingleChildScrollView(
+        child: Form(
+          key: formKey,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                ImcGauge(imc: imc),
+                const SizedBox(
+                  height: 20,
+                ),
+                SizedBox(
+                  width: 150,
+                  child: TextFormField(
+                    controller: pesoEC,
+                    keyboardType: TextInputType.number,
+                    decoration: const InputDecoration(labelText: 'Peso'),
+                    inputFormatters: [
+                      CurrencyTextInputFormatter(
+                          locale: 'pt_BR',
+                          symbol: '',
+                          decimalDigits: 2,
+                          turnOffGrouping: true),
+                    ],
+                    validator: (String? value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Peso obrigatório';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                SizedBox(
+                  width: 150,
+                  child: TextFormField(
+                    controller: alturaEC,
+                    keyboardType: TextInputType.number,
+                    decoration: const InputDecoration(labelText: 'Altura'),
+                    inputFormatters: [
+                      CurrencyTextInputFormatter(
+                          locale: 'pt_BR',
+                          symbol: '',
+                          decimalDigits: 2,
+                          turnOffGrouping: true),
+                    ],
+                    validator: (String? value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Altura obrigatório';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    var formValid = formKey.currentState?.validate() ?? false;
+
+                    if (formValid) {
+                      var formatter = NumberFormat.simpleCurrency(
+                          locale: 'pt_BR', decimalDigits: 2);
+                      double peso = formatter.parse(pesoEC.text) as double;
+                      double altura = formatter.parse(alturaEC.text) as double;
+
+                      _calcularIMC(peso: peso, altura: altura);
+                    }
+                  },
+                  child: const Text('Calcular IMC'),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/template.dart
@@ -0,0 +1,75 @@
+import 'package:app/widgets/imc_gauge)range.dart';
+import 'package:app/widgets/imc_gauge.dart';
+import 'package:currency_text_input_formatter/currency_text_input_formatter.dart';
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_gauges/gauges.dart';
+
+class ImcSetstatePage extends StatefulWidget {
+  const ImcSetstatePage({Key? key}) : super(key: key);
+
+  @override
+  State<ImcSetstatePage> createState() => _ImcSetstatePageState();
+}
+
+class _ImcSetstatePageState extends State<ImcSetstatePage> {
+  final pesoEC = TextEditingController();
+  final alturaEC = TextEditingController();
+
+  @override
+  void dispose() {
+    pesoEC.dispose();
+    alturaEC.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Imc SetState'),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              const ImcGauge(imc: 0),
+              const SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                controller: pesoEC,
+                keyboardType: TextInputType.number,
+                decoration: const InputDecoration(labelText: 'Peso'),
+                inputFormatters: [
+                  CurrencyTextInputFormatter(
+                      locale: 'pt_BR',
+                      symbol: '',
+                      decimalDigits: 2,
+                      turnOffGrouping: true),
+                ],
+              ),
+              TextFormField(
+                controller: alturaEC,
+                keyboardType: TextInputType.number,
+                decoration: const InputDecoration(labelText: 'Altura'),
+                inputFormatters: [
+                  CurrencyTextInputFormatter(
+                      locale: 'pt_BR',
+                      symbol: '',
+                      decimalDigits: 2,
+                      turnOffGrouping: true),
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                  onPressed: () {}, child: const Text('Calcular IMC'))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
